df3af8fcc2809516cf5c312fbcafe1cb40caff14
sandytan10 <stan10@stuy.edu>
2018-08-09 20:04:46 +0000

    Done.

--- summer_olympics_hash.rb
+++ summer_olympics_hash.rb
@@ -1,18 +1,42 @@
 
 def create_olympics_hash
-  # Implement this method so that it returns a hash with the data provided on README.md
+  {Sydney: "2000",
+  Athens: "2004",
+  Beijing: "2008",
+  London: "2012"}
+  end 
+ 
+def add_a_key_value_pair
+  olympics_hash = {Sydney: "2000",
+  Athens: "2004",
+  Beijing: "2008",
+  London: "2012"} 
+  olympics_hash[:Atlanta] = 1996
+  return olympics_hash
 end
 
-def add_a_key_value_pair
-  # Implement this method so that it adds a key value pair to the hash created in create_olympics_hash
-end
+puts add_a_key_value_pair
 
 def iterate_through_hash
-  # Implement this method so that it iterates over the hash created in add_a_key_value_pair
-  # and puts each key value pair within the phrase "The _____ summer olympics took place in _____."
+  city = ["Sydney", "Athens", "Beijing", "London"]
+  years = [2000, 2004, 2008, 2012]
+  summer_olympics = {}
+  summer_olympics.each do |city, year|
+  puts "The #{year} summer olympics took place in #{city}."
+  end
 end
+iterate_through_hash
 
 def iterate_through_keys
-  # Implement this method so that it converts the keys of the hash created in add_a_key_value_pair
-  # into uppercased Strings, creates a new array with these values, and then puts each upcase key in the CLI
+  cities = []
+  olympics_hash = {Sydney: "2000",
+  Athens: "2004",
+  Beijing: "2008",
+  London: "2012"} 
+  olympics_hash.each do |city, year|
+    cities.push(city.upcase)
 end
+  return cities 
+end 
+iterate_through_keys
+
